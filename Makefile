291e7673ab8f89bd0b03163969c7a22e69289975
Cesar Prados <bradomyn@gmail.com>
2017-05-20 13:55:01 +0200

    main: add Makefile, main and config file
    basic for testing the implemented classes

---
+++ Makefile
@@ -0,0 +1,29 @@
+CC = gcc
+CXX=g++
+SRC_DIR=src
+CFLAGS = -Wall -g
+INCLUDES = -I./$(SRC_DIR) 
+LFLAGS = 
+LIBS = 
+
+SRCS = $(wildcard $(SRC_DIR)/*.cpp)
+OBJS = $(SRCS:.c=.o)
+MAIN = wr-port-blocker
+
+.PHONY: depend clean
+
+all:    $(MAIN)
+
+$(MAIN): $(OBJS) 
+	$(CXX) $(CFLAGS) $(INCLUDES) -o $(MAIN) $(OBJS) $(LFLAGS) $(LIBS)
+
+.c.o:
+	$(CXX) $(CFLAGS) $(INCLUDES) -c $<  -o $@
+
+clean:
+	$(RM) *.o *~ $(MAIN)
+
+depend: $(SRCS)
+	makedepend $(INCLUDES) $^
+
+# DO NOT DELETE THIS LINE -- make depend needs it
---
+++ src/wr-port-blocker.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Conf.h"
+#include "Log.h"
+
+static void show_usage(std::string name)
+{
+    std::cerr << "Usage: " << name << " <option(s)> "
+              << "Options:\n"
+              << "\t-h,--help\tShow this help message\n"
+              << "\t-v,\t\tinfo verbosity level\n"
+              << "\t-vv,\t\tdebug verbosity level\n"
+              << "\t-c,--config  CONFIGFILE Specify path to config file\n"
+              << std::endl;
+}
+
+int main(int argc, char* argv[])
+{
+    if (argc < 1) {
+        show_usage(argv[0]);
+        return 1;
+    }
+
+    std::vector <std::string> sources;
+    std::string config_file;
+
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+        if ((arg == "-h") || (arg == "--help")) {
+            show_usage(argv[0]);
+            return 0;
+        } else if ((arg == "-c") || (arg == "--config")) {
+            if (i + 1 < argc) {
+                config_file = argv[i+1];
+            } else {
+                LogError("--config option requires one argument.");
+                return 1;
+            }  
+        } else if (arg == "-vv") {
+            SetLoggingLevel(LOG_DEBUG);
+            LogDebug("Verbosity Level: Debug");
+        } else if (arg == "-v") {
+            SetLoggingLevel(LOG_INFO);
+            LogInfo("Verbosity Level: Info");
+        }else {
+            sources.push_back(argv[i]);
+        }
+    }
+    
+    
+    // init 
+    Conf Conf(config_file);
+    if (Conf.GetConf() < 0) {
+        LogError("Without Config information, I don't start, Ciao");
+        return -1;
+    } else
+        LogInfo("Config File loaded correctly");
+    
+    //block = BlockerEngine(Conf);
+    //
+ 
+    return 0;
+}
