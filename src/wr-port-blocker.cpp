acd0fc139a33e6110f05e6f2895b6dd76847d1f9
C.Prados <c.prados@gsi.de>
2017-05-22 17:34:43 +0200

    main: add Ssh functionality for testing

--- src/wr-port-blocker.cpp
+++ src/wr-port-blocker.cpp
@@ -4,78 +4,75 @@
 #include "Conf.h"
 #include "Log.h"
 #include "Daemonize.h"
+#include "SshConn.h"
 
 LogLevel_t level = LOG_ERROR;
 
 static void show_usage(std::string name)
 {
-    std::cerr << "Usage: " << name << " <option(s)> "
-              << "Options:\n"
-              << "\t-h,--help\tShow this help message\n"
-              << "\t-v,\t\tinfo verbosity level\n"
-              << "\t-vv,\t\tdebug verbosity level\n"
-              << "\t-d \t\t daemonize"
-              << "\t-c,--config  CONFIGFILE Specify path to config file\n"
-              << std::endl;
+        std::cerr << "Usage: " << name << " <option(s)> "
+                << "Options:\n"
+                << "\t-h,--help\tShow this help message\n"
+                << "\t-v,\t\tinfo verbosity level\n"
+                << "\t-vv,\t\tdebug verbosity level\n"
+                << "\t-d \t\t daemonize"
+                << "\t-c,--config  CONFIGFILE Specify path to config file\n"
+                << std::endl;
 }
 
 int main(int argc, char* argv[])
 {
 
-    std::vector <std::string> sources;
-    std::string config_file;
-    int daemonize=0;
+        std::vector <std::string> sources;
+        std::string config_file;
+        int daemonize=0;
 
-    if (argc < 1) {
-        show_usage(argv[0]);
-        return 1;
-    }
-
-    for (int i = 1; i < argc; ++i) {
-        std::string arg = argv[i];
-        if ((arg == "-h") || (arg == "--help")) {
-            show_usage(argv[0]);
-            return 0;
-        } else if ((arg == "-c") || (arg == "--config")) {
-            if (i + 1 < argc) {
-                config_file = argv[i+1];
-            } else {
-                LogError("--config option requires one argument.");
+        if (argc < 1) {
+                show_usage(argv[0]);
                 return 1;
-            }
-        } else if (arg == "-d") {
-            LogInfo("Deamonize");
-            daemonize = 1;
-        } else if (arg == "-vv") {
-            SetLoggingLevel(LOG_DBG);
-            LogDebug("Verbosity Level: Debug");
-        } else if (arg == "-v") {
-            SetLoggingLevel(LOG_MSG);
-            LogInfo("Verbosity Level: Info");
-        }else {
-            sources.push_back(argv[i]);
         }
-    }
+
+        for (int i = 1; i < argc; ++i) {
+                std::string arg = argv[i];
+                if ((arg == "-h") || (arg == "--help")) {
+                        show_usage(argv[0]);
+                        return 0;
+                } else if ((arg == "-c") || (arg == "--config")) {
+                        if (i + 1 < argc) {
+                                config_file = argv[i+1];
+                        } else {
+                                LogError("--config option requires one argument.");
+                                return 1;
+                        }
+                } else if (arg == "-d") {
+                        LogInfo("Deamonize");
+                        daemonize = 1;
+                } else if (arg == "-vv") {
+                        SetLoggingLevel(LOG_DBG);
+                        LogDebug("Verbosity Level: Debug");
+                } else if (arg == "-v") {
+                        SetLoggingLevel(LOG_MSG);
+                        LogInfo("Verbosity Level: Info");
+                }else {
+                        sources.push_back(argv[i]);
+                }
+        }
 
 
-    // init
-    Conf Conf(config_file);
-    if (Conf.GetConf() < 0) {
-        LogError("Without Config information, I don't start, Ciao");
-        return -1;
-    } else
-        LogInfo("Config File loaded correctly");
+        // init
+        Conf Conf(config_file);
+        if (Conf.GetConf() < 0) {
+                LogError("Without Config information, I don't start, Ciao");
+                return -1;
+        } else
+                LogInfo("Config File loaded correctly");
 
-    if (daemonize)
-        Daemonize();
+        if (daemonize)
+                Daemonize();
 
-    while(1)
-    {
-        LogInfo("working");
-    }
+        SshConn SshConn(level);
+        SshConn.Config();
+        SshConn.SendCommand();
 
-    //block = BlockerEngine(Conf);
-    //
-
-    return 0;
+        return 0;
 }
