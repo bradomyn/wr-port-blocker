f06d53d3cc292c817a6b08b1296fca41870fedbc
C.Prados <c.prados@gsi.de>
2017-06-09 14:43:51 +0200

    log: add syslog to the log class

--- src/Log.h
+++ src/Log.h
@@ -1,15 +1,26 @@
-#ifndef Log
-#define Log
+#ifndef LOG_H
+#define LOG_H
 
 #include <iostream>
 #include <string>
+#include <syslog.h>
 
 typedef enum LogLevels { LOG_QUIET = 1, LOG_ERROR, LOG_MSG, LOG_DBG } LogLevel_t;
+//extern LogLevel_t level;
 
-extern LogLevel_t level;
+class Log
+{
 
-void SetLoggingLevel(int l);
-void LogInfo(std::string msg);
-void LogDebug(std::string msg);
-void LogError(std::string msg);
+
+        public:
+                Log(int DefaultLevel);
+                ~Log();
+                void SetLoggingLevel(int l);
+                void LogInfo(std::string msg);
+                void LogDebug(std::string msg);
+                void LogError(std::string msg);
+
+        private:
+                LogLevel_t level;
+};
 #endif
